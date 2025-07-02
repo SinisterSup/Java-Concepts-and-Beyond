89931f83f8d7ca7270d3d23e2b3dfde2b139cc92
SinisterSup <mheist456@gmail.com>
2025-07-02 20:44:31 +0530

    refactor MergeSorter to use multi-threading

--- src/concurrency2/Client.java
+++ src/concurrency2/Client.java
@@ -10,12 +10,21 @@ public class Client {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         List<Integer> nums = List.of(5, 2, 6, 1, 4, 7, 8, 3, 5, 4, 2, 3, 9);
 
-        // Normal mergeSort without multithreading
-        MergeSorter mergeSorter = new MergeSorter(nums);
-        List<Integer> sortedNums = mergeSorter.sort();
-        for (Integer num : sortedNums) {
+//        // Normal mergeSort without multithreading
+//        MergeSorter mergeSorter = new MergeSorter(nums);
+//        List<Integer> sortedNums = mergeSorter.sort();
+//        for (Integer num : sortedNums) {
+//            System.out.printf("%d ", num);
+//        }
+
+        // implementing multi-threading mergeSort using fixedThreadPool
+        ExecutorService executorService = Executors.newFixedThreadPool(13);
+        MergeSorter mergeSorter = new MergeSorter(nums, executorService);
+        Future<List<Integer>> listFuture = executorService.submit(mergeSorter);
+        for (Integer num : listFuture.get()) {
             System.out.printf("%d ", num);
         }
+        executorService.shutdown();
 
     }
 }
--- src/concurrency2/MergeSorter.java
+++ src/concurrency2/MergeSorter.java
@@ -3,27 +3,47 @@ package concurrency2;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 public class MergeSorter implements Callable<List<Integer>> {
     private final List<Integer> integerList;
+    private final ExecutorService executorService;
 
-    public MergeSorter(List<Integer> integerList) {
+//    public MergeSorter(List<Integer> integerList) {
+//        this.integerList = integerList;
+//    }
+    public MergeSorter(List<Integer> integerList, ExecutorService executorService) {
         this.integerList = integerList;
+        this.executorService = executorService;
     }
 
-    public List<Integer> sort() {
+    public List<Integer> sort() throws ExecutionException, InterruptedException {
         System.out.printf("Sorting %s on Thread %s\n", integerList, Thread.currentThread().getName());
         if (integerList.size() <= 1) return integerList;
         int mid = (integerList.size()) / 2;
         List<Integer> leftList = integerList.subList(0, mid);
         List<Integer> rightList = integerList.subList(mid, integerList.size());
 
+//        MergeSorter mergeSorterLeft = new MergeSorter(leftList);
+//        MergeSorter mergeSorterRight = new MergeSorter(rightList);
         MergeSorter mergeSorterLeft = new MergeSorter(leftList, executorService);
         MergeSorter mergeSorterRight = new MergeSorter(rightList, executorService);
 
-        List<Integer> sortedLeft = mergeSorterLeft.sort();
-        List<Integer> sortedRight = mergeSorterRight.sort();
+
+//        List<Integer> sortedLeft = mergeSorterLeft.sort();
+//        List<Integer> sortedRight = mergeSorterRight.sort();
+
+        // Instead of directly calling sort on the same thread,
+        // Submit the task to the thread-pool via executorService
+        // which in-turn schedules it on different threads.
+        Future<List<Integer>> futureLeftList = executorService.submit(mergeSorterLeft);
+        Future<List<Integer>> futureRightList = executorService.submit(mergeSorterRight);
+
+        // Blocking call to get the sorted array halves.
+        List<Integer> sortedLeft = futureLeftList.get();
+        List<Integer> sortedRight = futureRightList.get();
 
         // merge sorted halves
         int i = 0, j = 0;
@@ -45,7 +65,6 @@ public class MergeSorter implements Callable<List<Integer>> {
             mergedList.add(sortedRight.get(j));
             j++;
         }
-
         return  mergedList;
     }
 
