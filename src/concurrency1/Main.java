1790a5b3f3a28b3c822a39eef169ac5f80250475
SinisterSup <mheist456@gmail.com>
2025-07-02 19:50:48 +0530

    learnt Callable basics

--- src/concurrency1/Main.java
+++ src/concurrency1/Main.java
@@ -1,12 +1,18 @@
 package concurrency1;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         System.out.println("Hello World");
         Thread thread = Thread.currentThread();
 
         System.out.println(Thread.currentThread().getName());
 
+        // Runnable Demo
         for  (int i = 0; i < 100; i++) {
             NumberPrinter numPrinter = new NumberPrinter(i);
 //            numPrinter.run();
@@ -14,5 +20,19 @@ public class Main {
             newThread.start();
         }
 
+        // Callable demo
+        NumberDoubler numDoubler  = new NumberDoubler(100);
+        ExecutorService fixedExecutorService = Executors.newFixedThreadPool(2);
+        Future<Integer> integerFuture = fixedExecutorService.submit(numDoubler);
+
+        do {
+            Thread.sleep(200);
+            System.out.println(integerFuture.isDone());
+        } while (!integerFuture.isDone());
+
+        // Blocking call -> Main thread won't proceed until
+        // the thread completes execution for integer future.
+        System.out.println(integerFuture.get());
+        fixedExecutorService.shutdown();
     }
 }
---
+++ src/concurrency1/NumberDoubler.java
@@ -0,0 +1,15 @@
+package concurrency1;
+
+import java.util.concurrent.Callable;
+
+public class NumberDoubler implements Callable<Integer> {
+    private final int i;
+    public NumberDoubler(int i) {
+        this.i = i;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return i * 2;
+    }
+}
