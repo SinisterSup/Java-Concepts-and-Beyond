9d1ddc8c0df838d66931f23a0d56e33f3bc5d5a8
SinisterSup <mheist456@gmail.com>
2025-07-23 08:24:46 +0530

    add Comparator Examples

---
+++ src/generics/InventoryLab/ItemQuantityComparator.java
@@ -0,0 +1,13 @@
+package generics.InventoryLab;
+
+import java.util.Comparator;
+
+public class ItemQuantityComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item item1, Item item2) {
+        return Integer.compare(item1.getQuantity(), item2.getQuantity());
+//        if (item1.getQuantity() < item2.getQuantity()) return -1;
+//        else if (item1.getQuantity() > item2.getQuantity()) return 1;
+//        else return 0;
+    }
+}
